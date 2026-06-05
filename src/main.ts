92ad0857905bf6178585b7ca1a923d1294738c9d
elfplayer937187 <elfplayer937187@gmail.com>
2026-06-05 16:43:54 +0800

    路由守卫完成

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router/index.ts'
+import './router/permission'
 import globalComponents from './components/index.ts'
 import '@/styles/index.scss'
 
