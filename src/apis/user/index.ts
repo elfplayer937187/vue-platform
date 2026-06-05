86d66413aea0b60259e35954c06a6c8162539553
elfplayer937187 <elfplayer937187@gmail.com>
2026-06-05 21:43:23 +0800

    真接口api类型定义

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,6 +1,6 @@
 // 管理用户相关的请求
 import request from "@/utils/request";
-import type { LoginResponseType, LoginType,userResponseData } from "./type";
+import type { LoginType,ReqUserInfoType,ReqUserLoginType } from "./type";
 // 管理接口
 enum API{
     // 登录接口
@@ -11,6 +11,9 @@ enum API{
     LOGOUT_URL='/admin/acl/index/logout'
 }
 // 登录接口
-export const reqLogin=(data:any)=>request.post<any,any>(API.LOGIN_URL,data)
+// request.post<any,LoginType>第一个泛型是请求参数类型，第二个是响应类型
+export const reqLogin=(data:LoginType)=>request.post<any,ReqUserLoginType>(API.LOGIN_URL,data)
 // 得到用户信息接口
-export const reqUserInfo=()=>request.get<any,any>(API.USERINFO_URL)
+export const reqUserInfo=()=>request.get<any,ReqUserInfoType>(API.USERINFO_URL)
+// 退出登录接口
+export const reqUserLogout=(token:string)=>request.post<any,ReqUserLoginType>(token)
