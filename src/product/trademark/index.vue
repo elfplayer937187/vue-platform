f714d84b34e757a69bfbdbbf4ad945428a214354
elfplayer937187 <elfplayer937187@gmail.com>
2026-06-07 21:27:39 +0800

    feat: 品牌管理新增抽屉对话框

--- src/product/trademark/index.vue
+++ src/product/trademark/index.vue
@@ -1,7 +1,24 @@
 <template>
   <el-card class="box-card">
     <!-- 添加品牌按钮 -->
-    <el-button type="primary" icon="Plus" class="Plus">添加品牌</el-button>
+    <el-button type="primary" icon="Plus" class="Plus" @click="dialogVisible=true">添加品牌</el-button>
+    <!-- 对话框组件 -->
+    <el-dialog v-model="dialogVisible" title="添加品牌" width="500" :before-close="handleClose" draggable overflow>
+      <el-form label-width="80px">
+          <el-form-item label="文件名称" required>
+            <el-input v-model="tradeName" placeholder="请填写品牌名称"></el-input>
+          </el-form-item>
+          <el-form-item label="文件地址" required>
+
+          </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="dialogVisible = false"> Confirm </el-button>
+        </div>
+      </template>
+    </el-dialog>
     <!-- 表格组件 -->
     <!-- 
     table
@@ -14,23 +31,23 @@
       <!-- colomn里的prop以div形式展示东西 ，一般不用，用slot-->
       <el-table-column label="序号" width="width" text="index" align="center">
         <!-- 可以用插槽传递两个参数，第一个是该行信息，一个是索引 -->
-        <template v-slot="{$index}">
-          {{ $index+1 }}
+        <template v-slot="{ $index }">
+          {{ $index + 1 }}
         </template>
       </el-table-column>
       <el-table-column prop="prop" label="品牌名称" width="width">
-        <template #default="{row}">
+        <template #default="{ row }">
           {{ row.tmName }}
         </template>
       </el-table-column>
-      <el-table-column prop="prop" label="品牌logo" >
-        <template #default="{row}">
-          <img :src="row.logoUrl" alt="" style="width: 100px;height: 100px;">
+      <el-table-column prop="prop" label="品牌logo">
+        <template #default="{ row }">
+          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
         </template>
       </el-table-column>
       <el-table-column prop="prop" label="品牌操作" width="width">
-          <el-button type="primary" icon="Edit"></el-button>
-          <el-button type="primary" icon="Delete"></el-button>
+        <el-button type="primary" icon="Edit"></el-button>
+        <el-button type="primary" icon="Delete"></el-button>
       </el-table-column>
     </el-table>
     <!-- 分页器 -->
@@ -41,7 +58,7 @@
         v-model:page-size="pageSize"
         :background="background"
         layout="total,prev, pager, next,->,sizes, jumper"
-        :page-sizes="[3,4,5,6]"
+        :page-sizes="[3, 4, 5, 6]"
         :total="total"
         prev-text="上一页"
         next-text="下一页"
@@ -55,7 +72,9 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import { getTrademarkList } from '@/apis/product/trademark'
-import type { RecordsType,getTrademarkListResponseType } from '@/apis/product/trademark/type'
+import type { RecordsType, getTrademarkListResponseType } from '@/apis/product/trademark/type'
+// 对话框是否可见
+const dialogVisible=ref<boolean>(false)
 // 当前页面码
 const currentPage = ref<number>(1)
 // 一个页面显示多少条数据
@@ -64,35 +83,43 @@ const pageSize = ref<number>(3)
 const background = ref(true)
 // 数据总数
 const total = ref<number>(0)
-const tradeMarkList = ref<RecordsType>([])  
-const HasTradeMark=async()=>{
-  const res:getTrademarkListResponseType=await getTrademarkList(currentPage.value,pageSize.value)
-  if(res.code===200){
-    console.log(res);
-    
-    total.value=res.data.total
-    tradeMarkList.value=res.data.records
+const tradeMarkList = ref<RecordsType>([])
+const HasTradeMark = async () => {
+  const res: getTrademarkListResponseType = await getTrademarkList(
+    currentPage.value,
+    pageSize.value,
+  )
+  if (res.code === 200) {
+    console.log(res)
+
+    total.value = res.data.total
+    tradeMarkList.value = res.data.records
   }
 }
-
-onMounted(()=>{
+// 初始化品牌名称和路径
+const tradeName=ref<string>('')
+onMounted(() => {
   HasTradeMark()
 })
 // 页面页数变化触发
-const ChangePage=()=>{
+const ChangePage = () => {
   HasTradeMark()
 }
-const ChangeSize=()=>{
-  currentPage.value=1
+// 页面尺寸变化触发
+const ChangeSize = () => {
+  currentPage.value = 1
   HasTradeMark()
+}
+const handleClose=()=>{
+
 }
 </script>
 
 <style scoped lang="scss">
-.demo-pagination-block{
+.demo-pagination-block {
   margin-top: 10px;
 }
-.Plus{
+.Plus {
   margin-bottom: 10px;
 }
 </style>
