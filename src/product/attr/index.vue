6238a1840676ef2013f3f69089f31a6d3b8e13cf
elfplayer937187 <elfplayer937187@gmail.com>
2026-06-11 21:47:24 +0800

    除了删除操作,基本完成attr

--- src/product/attr/index.vue
+++ src/product/attr/index.vue
@@ -26,41 +26,66 @@
             </template>
           </el-table-column>
           <el-table-column prop="prop" label="操作" width="200px" align="center">
-            <el-button type="primary" icon="Edit"></el-button>
-            <el-button type="primary" icon="Delete"></el-button>
+            <template #default="{ row }">
+              <el-button type="primary" icon="Edit" @click="HandleEdit(row)"></el-button>
+              <el-button type="primary" icon="Delete"></el-button>
+            </template>
           </el-table-column>
         </el-table>
       </div>
       <div v-show="showChange">
         <!-- 添加元素静态组件 -->
-         <el-form ref="form">
-             <el-form-item label="属性名称">
-              <el-input placeholder="请输入文本" style="width: 300px;" v-model="AppendParams.attrName"></el-input>
-             </el-form-item>
-             <el-form-item label="">
-              <el-button @click="appendAttr" type="primary" class="append-attr" icon="Plus" :disabled="!AppendParams.attrName.trim()">添加属性值</el-button>
-              <el-button type="primary" @click="HandleCancel">取消</el-button>
-             </el-form-item>
-             <el-form-item label="">
-                <el-table :data="AppendParams.attrValueList" style="width: 100%" border>
-                    <el-table-column prop="prop" label="序号" width="100px" align="center" type="index">
-                    </el-table-column>
-                    <el-table-column prop="prop" label="属性值" width="width">
-                      <template #default="{row}">
-                        <el-input v-model="row.valueName" placeholder="请输入属性" class="attr-append"></el-input>
-                      </template>
-                    </el-table-column>
-                    <el-table-column prop="prop" label="操作" width="200px" align="center">
-                      <el-button type="primary" icon="Edit"></el-button>
-                      <el-button type="primary" icon="Delete"></el-button>
-                    </el-table-column>
-                </el-table>
-              <el-form-item label="" class="under-button">
-                <el-button type="primary">保存</el-button>
-                <el-button >取消</el-button>
-              </el-form-item>
-             </el-form-item>
-         </el-form>
+        <el-form ref="form">
+          <el-form-item label="属性名称">
+            <el-input
+              placeholder="请输入文本"
+              style="width: 300px"
+              v-model="AppendParams.attrName"
+            ></el-input>
+          </el-form-item>
+          <el-form-item label="">
+            <el-button
+              @click="appendAttr"
+              type="primary"
+              class="append-attr"
+              icon="Plus"
+              :disabled="AppendParams.attrName.trim().length === 0"
+              >添加属性值</el-button
+            >
+            <el-button type="primary" @click="HandleCancel">取消(不保留数据)</el-button>
+          </el-form-item>
+          <el-form-item label="">
+            <el-table :data="AppendParams.attrValueList" style="width: 100%" border>
+              <el-table-column prop="prop" label="序号" width="100px" align="center" type="index">
+              </el-table-column>
+              <el-table-column prop="prop" label="属性值" width="width">
+                <template #default="{ row }">
+                  <el-input
+                    v-model="row.valueName"
+                    placeholder="请输入属性"
+                    class="attr-append"
+                  ></el-input>
+                </template>
+              </el-table-column>
+              <el-table-column prop="prop" label="操作" width="200px" align="center">
+                <template #default="{ row }">
+                  <el-button
+                    type="primary"
+                    icon="Delete"
+                    @click="handleDelete2(row.valueName)"
+                  ></el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+            <el-form-item label="" class="under-button">
+              <!-- disabled:数组为空 -->
+              <el-button type="primary" @click="saveAppendParams" :disabled="EachValueNameNotNull()"
+                >保存</el-button
+              >
+              <el-button @click="HandleSaveCancel">取消(保留数据)</el-button>
+            </el-form-item>
+          </el-form-item>
+        </el-form>
       </div>
     </el-card>
   </div>
@@ -71,13 +96,14 @@ import Category from '@/components/Category/index.vue'
 import useCategoryStore from '@/stores/modules/Category'
 import { watch, ref, reactive } from 'vue'
 import { storeToRefs } from 'pinia'
-import { getCategoryTag } from '@/apis/product/attr'
+import { appendThridTag, getCategoryTag } from '@/apis/product/attr'
 import type { ListItemType } from '@/apis/product/attr/type'
+import 'element-plus/dist/index.css'
+import { ElMessage } from 'element-plus'
 const CategoryStore = useCategoryStore()
-const { C1Id, C2Id, C3Id,Isdisabled } = storeToRefs(CategoryStore)
+const { C1Id, C2Id, C3Id, Isdisabled } = storeToRefs(CategoryStore)
 // 修改页面是否展示
 const showChange = ref<boolean>(false)
-
 
 // 收集新增数据
 /* 
@@ -86,48 +112,50 @@ const showChange = ref<boolean>(false)
   1.新增一个对象到attrValueList,{属性值名称}
   2.v-for动态渲染table
 */
-
-const AppendParams=reactive<ListItemType>({
-  attrName:'',
-  categoryId:C3Id.value,
-  categoryLevel:3,
+// 存储的收集第二界面表单的元素
+const AppendParams = reactive<ListItemType>({
+  attrName: '',
+  categoryId: C3Id.value,
+  categoryLevel: 3,
   // 初始设定为空数组
-  attrValueList:[]
+  attrValueList: [],
 })
+// 判断AppendParams里面的list的属性都不为空值
+// 判断是否可以禁用保存按钮：数组为空或有空值 → 禁用
+const EachValueNameNotNull = () => {
+  if (AppendParams.attrValueList.length === 0) {
+    return true // 数组为空 → 禁用
+  }
+  // 只要有一个 valueName 是空（含纯空格），就禁用
+  return AppendParams.attrValueList.some((item) => item.valueName.trim() === '')
+}
 // 添加新元素
-const appendAttr=()=>{
+const appendAttr = () => {
   AppendParams.attrValueList.push({
-    valueName:''
+    valueName: '',
   })
+  // 改变categoryId
+  AppendParams.categoryId = C3Id.value
 }
 // 添加元素显示另一个卡片的点击事件
 const AppendValue = () => {
   // 点击切换菜单，并且禁用按钮
   showChange.value = true
-  Isdisabled.value=true
+  Isdisabled.value = true
 }
-// 处理取消
-const HandleCancel=()=>{
-  // 改变视图并且让初始卡片看得见
-  showChange.value=false
-  Isdisabled.value=false
-}
+
 const AttrList = ref<ListItemType[]>([])
 // 监视第三项api,如果有一个变了说明列表改变
 watch(C3Id, () => {
   // 得到元素，存储到AttrList,然后加载界面
   getAttr()
-  // 改变categoryId
-  AppendParams.categoryId=C3Id.value
 })
-// 获取属性值
+// 获取第一界面属性值列表
 const getAttr = async () => {
   try {
     // console.log('##',C1Id.value,C2Id.value,C3Id.value);
     const res = await getCategoryTag(C1Id.value, C2Id.value, C3Id.value)
     if (res.code === 200) {
-      console.log(res)
-
       AttrList.value = res.data
     } else {
       throw '响应失败'
@@ -137,16 +165,74 @@ const getAttr = async () => {
     throw '网络异常'
   }
 }
+// 处理第一界面编辑
+const HandleEdit = (row:any) => {
+  // 跳转第二界面,附上该赋值的值，并且把id传给appendParams
+  showChange.value = true
+  // 深拷贝实现,浅拷贝会有标签bug
+  Object.assign(AppendParams,JSON.parse(JSON.stringify(row)))
+}
+// 保存第二界面值
+const saveAppendParams = async () => {
+  // 收集参数发请求
+  try {
+    const res = await appendThridTag(AppendParams)
+    if (res.code === 200) {
+      ElMessage({
+        type: 'success',
+        message: '保存成功',
+      })
+      showChange.value = false
+      // 重新渲染第一界面列表
+      await getAttr()
+      return 'ok'
+    } else {
+      ElMessage({
+        type: 'error',
+        message: '保存失败',
+      })
+      throw '参数异常'
+    }
+  } catch {
+    ElMessage({
+      type: 'error',
+      message: '保存失败',
+    })
+    throw '网络异常'
+  }
+}
+// 处理退出第二界面
+const HandleSaveCancel = async () => {
+  // 改变视图并且让初始卡片看得见
+  showChange.value = false
+  Isdisabled.value = false
+  // 重新渲染第一界面列表
+  await getAttr()
+}
+// 取消第二界面值
+const HandleCancel = () => {
+  // attrName,attrValueList->null
+  AppendParams.attrName = ''
+  AppendParams.attrValueList = []
+  showChange.value = false
+  Isdisabled.value = false
+}
+// 第二界面删除tag函数
+const handleDelete2 = (name: string) => {
+  AppendParams.attrValueList = AppendParams.attrValueList.filter((item) => {
+    return item.valueName !== name
+  })
+}
 </script>
 
 <style scoped lang="scss">
 .tagg {
   margin-right: 10px;
 }
-.under-button{
+.under-button {
   margin-top: 10px;
 }
-.attr-append{
+.attr-append {
   width: 500px;
 }
 </style>
