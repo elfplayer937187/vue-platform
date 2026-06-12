268afbcd897721cd90a7930300b03d51ab8f4dd1
elfplayer937187 <elfplayer937187@gmail.com>
2026-06-12 15:34:05 +0800

    attr部分完成

--- src/apis/product/attr/index.ts
+++ src/apis/product/attr/index.ts
@@ -1,5 +1,5 @@
 import request from "@/utils/request";
-import type {ListItemType, ResponseAttrType,getAttrInfoListResponseType} from "./type";
+import type {ListItemType, ResponseAttrType,getAttrInfoListResponseType,deleteThirdResponseType} from "./type";
 enum API{
     // 获取第n个下拉框的接口
     CATEGORYONE_URL="/admin/product/getCategory1",
@@ -8,11 +8,15 @@ enum API{
     // 获取属性tag标签接口
     CATEGORYTAG_URL='/admin/product/attrInfoList',
     // 新增修改三级属性标签接口
-    APPENDTHIRDTAG_URL='/admin/product/saveAttrInfo'
+    APPENDTHIRDTAG_URL='/admin/product/saveAttrInfo',
+    DELETETHIRDATTR_URL="/admin/product/deleteAttr"
 }
 // 一级分类地址
 export const getFirstCategory=()=>request.get<any,ResponseAttrType>(API.CATEGORYONE_URL)
 export const getSecondCategory=(id:number|string)=>request.get<any,ResponseAttrType>(API.CATEGORYTWO_URL+`/${id}`)
 export const getThirdCategory=(id:number|string)=>request.get<any,ResponseAttrType>(API.CATEGORYTHREE_URL+`/${id}`)
 export const getCategoryTag=(C1id:string|number,C2Id:string|number,C3Id:string|number)=>request.get<any,getAttrInfoListResponseType>(API.CATEGORYTAG_URL+`/${C1id}/${C2Id}/${C3Id}`)
+// 添加三级属性标签接口
 export const appendThridTag=(obj:ListItemType)=>request.post<any,any>(API.APPENDTHIRDTAG_URL,obj)
+// 删除属性接口
+export const deleteThirdCategory=(attrId:number)=>request.delete<any,deleteThirdResponseType>(API.DELETETHIRDATTR_URL+`/${attrId}`)
