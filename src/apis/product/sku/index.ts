52bc3975a5185aba0f7bcce0930da1f5d6008643
elfplayer937187 <elfplayer937187@gmail.com>
2026-07-15 17:21:54 +0800

    feat: 商品管理模块整体完成

--- src/apis/product/sku/index.ts
+++ src/apis/product/sku/index.ts
@@ -1,5 +1,5 @@
 import request from '@/utils/request'
-import type { SKUType,DefaultResponseType,SKUResponseType, skuPaginationResponseType } from './type'
+import type { SKUType,DefaultResponseType,SKUResponseType, skuPaginationResponseType,SKUInfoResponseType } from './type'
 enum API {
   // 新增SKU
   APPENDSKUINFO_URL = '/admin/product/saveSkuInfo',
@@ -8,7 +8,13 @@ enum API {
   // 获取SKU分页列表
   GETSKUPAGINATION_URL='/admin/product/list',
   // 删除SKU接口
-  DELETESKUINFO_URL='/admin/product/deleteSku'
+  DELETESKUINFO_URL='/admin/product/deleteSku',
+  // 上架SKU接口
+  ONSALE_URL='/admin/product/onSale',
+  // 下架SKU接口
+  CANCELSALE_URL='/admin/product/cancelSale',
+  // 获取商品详情接口
+  GETSKUINFO_URL='/admin/product/getSkuInfo'
 }
 // 添加sku接口
 export const reqAppendSKUInfo = (obj: SKUType) => request.post<string, DefaultResponseType>(API.APPENDSKUINFO_URL, obj)
@@ -18,3 +24,9 @@ export const reqShowSKUInfo=(spuId:number)=>request.get<string,SKUResponseType>(
 export const reqGetSKUPagination=(page:number,limit:number)=>request.get<string,skuPaginationResponseType>(API.GETSKUPAGINATION_URL+`/${page}/${limit}`)
 // 删除sku接口
 export const reqDeleteSKU=(skuId:number)=>request.delete<string,DefaultResponseType>(API.DELETESKUINFO_URL+`/${skuId}`)
+// 上架sku接口
+export const reqOnSale=(skuId:number)=>request.get<string,DefaultResponseType>(API.ONSALE_URL+`/${skuId}`)
+// 下架sku接口
+export const reqCancelSale=(skuId:number)=>request.get<string,DefaultResponseType>(API.CANCELSALE_URL+`/${skuId}`)
+// 获取商品详情接口
+export const reqGetSKUInfo=(skuId:number)=>request.get<string,SKUInfoResponseType>(API.GETSKUINFO_URL+`/${skuId}`)
--- src/product/sku/index.vue
+++ src/product/sku/index.vue
@@ -1,41 +1,55 @@
 <template>
-  <el-table :data="SKUList" style="width: 100%" border>
-    <el-table-column prop="prop" label="序号" width="100px" align="center" type="index"></el-table-column>
-    <el-table-column prop="prop" label="名称" width="width">
-      <template #default="{row}">
-        <span>{{ row.skuName }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="prop" label="描述" width="width">
-      <template #default="{row}">
-        <span>{{ row.skuDesc }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="prop" label="默认图片" width="width">
-      <template #default="{row}">
-        <el-image style="width: 100px; height: 100px" :src="row.skuDefaultImg"></el-image>
-      </template>
-    </el-table-column>
-    <el-table-column prop="prop" label="重量(g)" width="width">
-      <template #default="{row}">
-        <span>{{ row.weight }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="prop" label="价格(元)" width="width"> 
-      <template #default="{row}">
-        <span>{{ row.price }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column prop="prop" label="操作" width="400px">
-      <template #default="{row}">
-        <el-button type="success" icon="Top">上架</el-button>
-        <el-button type="primary" icon="Edit">编辑</el-button>
-        <el-button type="warning" icon="InfoFilled">查看</el-button>
-        <el-button type="danger" icon="Delete" @click="deleteSKU(row)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <!-- 分页器 -->
+  <div>
+    <el-table :data="SKUList" style="width: 100%" border>
+      <el-table-column
+        prop="prop"
+        label="序号"
+        width="100px"
+        align="center"
+        type="index"
+      ></el-table-column>
+      <el-table-column prop="prop" label="名称" width="width">
+        <template #default="{ row }">
+          <span>{{ row.skuName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="描述" width="width">
+        <template #default="{ row }">
+          <span>{{ row.skuDesc }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="默认图片" width="width">
+        <template #default="{ row }">
+          <el-image style="width: 100px; height: 100px" :src="row.skuDefaultImg"></el-image>
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="重量(g)" width="width">
+        <template #default="{ row }">
+          <span>{{ row.weight }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="价格(元)" width="width">
+        <template #default="{ row }">
+          <span>{{ row.price }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="prop" label="操作" width="400px">
+        <template #default="{ row }">
+          <el-button
+            type="success"
+            :icon="row.isSale === 0 ? 'Bottom' : 'Top'"
+            @click="ChangeOnSale(row.isSale, row.id)"
+            >上架</el-button
+          >
+          <el-button type="primary" icon="Edit">编辑</el-button>
+          <el-button type="warning" icon="InfoFilled" @click="ShowSKUDetail(row.id)"
+            >查看</el-button
+          >
+          <el-button type="danger" icon="Delete" @click="deleteSKU(row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 分页器 -->
     <el-pagination
       v-model:current-page="pageNum"
       v-model:page-size="pageSize"
@@ -46,59 +60,148 @@
       @change="getSKUPagination()"
       :size="size"
     />
+    <!-- 抽屉栏 -->
+    <el-drawer v-model="ShowDrawer">
+      <template #header>
+        <h4>查看商品详情</h4>
+      </template>
+      <template #default>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">名称</el-col>
+          <el-col :span="18">{{ SKUInfo?.skuName }}</el-col>
+        </el-row>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">描述</el-col>
+          <el-col :span="18">{{ SKUInfo?.skuDesc }}</el-col>
+        </el-row>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">价格</el-col>
+          <el-col :span="18">{{ SKUInfo?.price }}</el-col>
+        </el-row>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">平台属性</el-col>
+          <el-col :span="18">
+            <el-tag
+              type="success"
+              v-for="SaleAttr in SKUInfo?.skuAttrValueList"
+              :key="SaleAttr.id"
+              >{{ SaleAttr.attrName }}</el-tag
+            >
+          </el-col>
+        </el-row>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">销售属性</el-col>
+          <el-col :span="18">
+            <el-tag
+              type="warning"
+              v-for="AttrSale in SKUInfo?.skuSaleAttrValueList"
+              :key="AttrSale.id"
+              >{{ AttrSale.saleAttrName }}</el-tag
+            >
+          </el-col>
+        </el-row>
+        <el-row :gutter="15" type="flex">
+          <el-col :span="6">商品图片</el-col>
+          <el-col :span="18">
+            <el-carousel indicator-position="outside" height="120px" :autoplay='true' :interval="3000">
+              <el-carousel-item>
+                <el-image style="width: 100px; height: 100px" :src="SKUInfo?.skuDefaultImg"></el-image>
+              </el-carousel-item>
+            </el-carousel>
+          </el-col>
+        </el-row>
+      </template>
+    </el-drawer>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { reqDeleteSKU, reqGetSKUPagination } from '@/apis/product/sku'
+import {
+  reqDeleteSKU,
+  reqGetSKUPagination,
+  reqOnSale,
+  reqCancelSale,
+  reqGetSKUInfo,
+} from '@/apis/product/sku'
 import type { SKUType } from '@/apis/product/sku/type'
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import 'element-plus/dist/index.css' // 关键：引入所有组件样式
 
-
-type paginationSize='small'|'large'
-const total=ref<number>(100)
-const pageSize=ref<number>(4)
-const pageNum=ref<number>(1)
-const pageSizeList=[4,6,8,10]
-const size:paginationSize='large'
+type paginationSize = 'small' | 'large'
+const total = ref<number>(100)
+const pageSize = ref<number>(4)
+const pageNum = ref<number>(1)
+const pageSizeList = [4, 6, 8, 10]
+const size: paginationSize = 'large'
+// 是否显示Drawer
+const ShowDrawer = ref<boolean>(false)
 // 存储表格信息
-const SKUList=ref<SKUType[]>([])
-  // 获取列表信息
-const getSKUPagination=async(Notify:boolean=true)=>{
+const SKUList = ref<SKUType[]>([])
+// 存储SKU详细信息
+const SKUInfo = ref<SKUType>()
+// 获取列表信息
+const getSKUPagination = async (Notify: boolean = true) => {
+  const res = await reqGetSKUPagination(pageNum.value, pageSize.value)
+  if (res.code === 200) {
+    SKUList.value = res.data.records
+    total.value = res.data.total
 
-    const res=await reqGetSKUPagination(pageNum.value,pageSize.value)
-    if(res.code===200){
-      SKUList.value=res.data.records
-      total.value=res.data.total
-      // 是否要展示ElMessage
-      if(Notify){
-        ElMessage({
-        type:'success',
-        message:'删除成功'
-      })
-      }
-      return 'ok'
-    }else{
+    // 是否要展示ElMessage
+    if (Notify) {
       ElMessage({
-        type:'error',
-        message:'删除失败'
+        type: 'success',
+        message: '删除成功',
       })
-      throw new Error(res.message||'获取列表失败')
     }
+    return 'ok'
+  } else {
+    ElMessage({
+      type: 'error',
+      message: '删除失败',
+    })
+    throw new Error(res.message || '获取列表失败')
+  }
 }
 // 挂载时获取列表信息
-onMounted(async()=>{
+onMounted(async () => {
   // 获取分页信息
   await getSKUPagination(false)
-
 })
 // 删除sku信息
-const deleteSKU=async(row:SKUType)=>{
-  const res=await reqDeleteSKU((row.id as number))
-  console.log(res);
+const deleteSKU = async (row: SKUType) => {
+  const res = await reqDeleteSKU(row.id as number)
+  console.log(res)
   // 重新获取SKU
   await getSKUPagination()
+}
+// 控制是否上架
+const ChangeOnSale = async (isSale: number, skuId: number) => {
+  if (isSale) {
+    await reqCancelSale(skuId)
+    ElMessage({
+      type: 'success',
+      message: '成功下架',
+    })
+  } else {
+    await reqOnSale(skuId)
+    ElMessage({
+      type: 'success',
+      message: '成功上架',
+    })
+  }
+  await getSKUPagination(false)
+}
+// 显示SKU详细信息
+const ShowSKUDetail = async (skuId: number) => {
+  ShowDrawer.value = true
+  // 获取详细信息
+  const res = await reqGetSKUInfo(skuId)
+  console.log(res)
+
+  if (res.code === 200) {
+    SKUInfo.value = res.data
+  }
 }
 </script>
 
@@ -106,5 +209,10 @@ const deleteSKU=async(row:SKUType)=>{
 .pagination {
   margin-top: 10px;
 }
-
+.el-drawer .el-row {
+  margin: 20px 0;
+}
+.el-row .el-image{
+  margin-left: 80px;
+}
 </style>
