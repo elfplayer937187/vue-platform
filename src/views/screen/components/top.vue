9ebae965e63a0a7b3ef6cb75debb51c6023586c8
elfplayer937187 <elfplayer937187@gmail.com>
2026-07-20 23:21:15 +0800

    feat:首页完成 完成顶部大屏界面并且实现适配

---
+++ src/views/screen/components/top.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="top">
+    <div class="left">
+      <span class="lbtn">首页</span>
+    </div>
+    <div class="center">智慧旅游可视化数据大平台</div>
+    <div class="right">
+      <span class="rbtn">统计报告</span>
+      <span class="time">当前时间: {{ now }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import dayjs from 'dayjs'
+import { onMounted, ref } from 'vue'
+const now = ref(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+onMounted(() => {
+  setInterval(()=>{
+    now.value=dayjs().format('YYYY-MM-DD HH:mm:ss')
+  },1000)
+})
+</script>
+
+<style scoped lang="scss">
+.top {
+  width: 100%;
+  height: 40px;
+  // background-color: pink;
+  display: flex;
+
+  .left {
+    flex: 1;
+    position: relative;
+    background-image: url(../images/dataScreen-header-left-bg.png);
+    background-repeat: no-repeat;
+
+    .lbtn {
+      position: absolute;
+      right: 0;
+      background-image: url(../images/dataScreen-header-btn-bg-l.png);
+      background-repeat: no-repeat;
+      width: 128px;
+      height: 100%;
+      line-height: 40px;
+      text-align: center;
+      color: skyblue;
+    }
+  }
+  .center {
+    width: 100%;
+    height: 70px;
+    flex: 2;
+    background-image: url(../images/dataScreen-header-center-bg.png);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    text-align: center;
+    line-height: 70px;
+    color: skyblue;
+    font-size: 30px;
+  }
+  .right {
+    flex: 1;
+    background-image: url(../images/dataScreen-header-right-bg.png);
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    // position: relative;
+    .rbtn {
+      // position: absolute;
+      // left: 0;
+      line-height: 40px;
+      height: 40px;
+      // display: block;
+      background-image: url(../images/dataScreen-header-btn-bg-r.png);
+      background-repeat: no-repeat;
+      width: 150px;
+      text-align: center;
+      color: skyblue;
+      margin-left: -5px;
+    }
+    .time {
+      text-align: center;
+      line-height: 40px;
+      color: skyblue;
+      margin-right: 20px;
+    }
+  }
+}
+</style>
--- src/views/screen/index.vue
+++ src/views/screen/index.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>screen</div>
+  <div class="container">
+    <div ref="Screen" class="content">
+      <Top></Top>
+      <div class="bottom">
+        <div class="left">left</div>
+        <div class="middle">middle</div>
+        <div class="right">right</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import Top from './components/top.vue'
+const Screen = ref()
 
-<style scoped lang="scss"></style>
+onMounted(() => {
+  Screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`
+})
+
+function getScale(w = 1920, h = 1080) {
+  const ww = window.innerWidth / w
+  const hh = window.innerHeight / h
+  return ww > hh ? hh : ww
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  height: 100vh;
+  width: 100vw;
+  background-image: url(./images/bg.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  .content {
+    width: 1920px;
+    height: 1080px;
+    // background-color: red;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform-origin: center center;
+  }
+}
+</style>
